b75675fe7bc871ef6dfac741efeb815a64903b7f
Joseph T. Ball <jball321@live.com>
2018-06-27 16:58:05 -0400

    Setting up file structure for a Wordpress theme.

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>P.J. Sweeney's</title>
+		<meta name="author" content="Joseph T. Ball">
+		<link rel="shortcut icon" href="" type="image/ico">
+    </head>
+
+    <body>
